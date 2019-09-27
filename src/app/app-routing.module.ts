19e4c01ea407f0eff530d978027f58505bf75c5c
Kara <fox.kara@gmail.com>
2019-09-27 10:57:07 -0700

    added home content, set up views for additional pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { DevComponent } from './views/dev/dev.component';
+import { ProductComponent } from './views/product/product.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'development', component: DevComponent },
+  { path: 'product', component: ProductComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { DevComponent } from './views/dev/dev.component';
+import { ProductComponent } from './views/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DevComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
